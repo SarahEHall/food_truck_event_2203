63218906d7c8f3557581ad24e479e2bc1d513a87
Sarah Hall <swatson1994@yahoo.com>
2022-04-26 11:25:09 -0400

    "Feat: Add check_stock and stock methods, make pass"

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -6,4 +6,18 @@ class FoodTruck
     @name = name
     @inventory = {}
   end
+
+  def check_stock(item)
+    if !@inventory.include?(item)
+      return 0
+    end
+  end
+
+  def stock(item, amount)
+    if @inventory.keys.include?(item)
+      @inventory[item] += amount
+    else
+      @inventory[item] = amount
+    end
+  end
 end
